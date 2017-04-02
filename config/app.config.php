d86b4d10ede3311db779ba644d1e2fc035e74501
EmixMaxime <emixmaxime@outlook.fr>
2017-04-02 22:11:56 +0200

    Adding configurations files

---
+++ config/app.config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+  'env' => env('APP_ENV', 'prod'),
+
+];
---
+++ config/view.config.php
@@ -0,0 +1,9 @@
+<?php 
+
+return [
+
+  'path' => resource_path('views'),
+
+  'compiled' => storage_path('framework/views'),
+
+];
