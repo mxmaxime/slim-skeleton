a632a11237e38631a0c1f0475de783639eb6e7be
EmixMaxime <emixmaxime@outlook.fr>
2017-04-04 22:52:56 +0200

    Refactoring helpers

--- Emix/helpers.php
+++ Emix/helpers.php
@@ -1,12 +1,5 @@
-<?php 
-
-if (!function_exists('config')) {
-
-  function config (String $config): String {
-    return '';
-  }
-
-}
+<?php
+use Emix\Support\PathHelpers;
 
 if (!function_exists('env')) {
   
@@ -17,49 +10,16 @@ if (!function_exists('env')) {
 
 }
 
-if (!function_exists('root_path')) {
-
-  function root_path () {
-    return realpath('../');
+if (!function_exists('resource_path')) {
+  function resource_path (String $path): String {
+    return PathHelpers::getInstance()->resources . '/' . $path;
   }
-
-}
-
-if (!function_exists('config_path')) {
-
-  function config_path () {
-    return realpath('../config');
-  }
-
 }
 
 if (!function_exists('storage_path')) {
 
   function storage_path (String $path): String {
-    return realpath("../");
-  }
-}
-
-if (!function_exists('resource_path')) {
-
-  function resource_path (String $path): String {
-    return realpath("../resources/$path");
-  }
-
-}
-
-if (!function_exists('public_path')) {
-
-  function public_path (): String {
-    return realpath('./');
-  }
-
-}
-
-if (!function_exists('storage_path')) {
-  
-  function storage_path (): String {
-    return '';
+    return PathHelpers::getInstance()->storage . '/' . $path;
   }
 
 }
